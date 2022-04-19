1ac3dfca325c922284b059ad71d80085e39b547a
sachinbadnikai <38239908+sachinbadnikai@users.noreply.github.com>
2022-04-19 12:14:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const express = require("express");
-const db = require("./config/database.js");
-const productRoutes = require("./routes/index.js");
-const cors = require("cors");
- 
+const express = require('express');
 const app = express();
- 
-try {
-    await db.authenticate();
-    console.log('Database connected...');
-} catch (error) {
-    console.error('Connection error:', error);
-}
- 
-app.use(cors());
-app.use(express.json());
-app.use('/products', productRoutes);
- 
-app.listen(5000, () => console.log('Server running at port 5000'));
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
