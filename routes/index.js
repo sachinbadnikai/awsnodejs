54dca2faf770288be3c3c4651a4bbc3f5268ae5e
sachinbadnikai <38239908+sachinbadnikai@users.noreply.github.com>
2022-04-19 11:45:17 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
-import express from "express";
- 
-import { 
-    createProduct
-} from "../controllers/Products.js";
+const express = require("express");
+const createProduct = require("../controllers/Products.js");
  
 const router = express.Router();
 
